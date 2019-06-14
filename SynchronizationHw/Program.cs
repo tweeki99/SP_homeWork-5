4a2ab25e858372701d39e747e8be9d1d03b4e062
Шипилов Данил Валерьевич <@corp.itstep>
2019-06-14 11:32:58 +0600

    SpHw-5

---
+++ SynchronizationHw/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SynchronizationHw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new User();
+            for (int i = 0; i < 10; i++)
+            {
+                var thread = new Thread(account.BeginTransaction);
+                thread.Start();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SynchronizationHw/User.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SynchronizationHw
+{
+    public class User
+    {
+        public int Money { get; set; } = 100000;
+        private object lockObject = new object();
+
+        public void BeginTransaction()
+        {
+                Console.WriteLine(Thread.CurrentThread.ManagedThreadId.ToString() + " начинает работу");
+                Random random = new Random();
+                for (int i = 0; i < 15; i++)
+                {
+                    int randomNumber = random.Next(100);
+
+                    if (randomNumber < 50)
+                        Money -= 15000;
+                    else
+                        Money += 15000;
+                    Console.WriteLine(Money);
+                }
+                Console.WriteLine(Thread.CurrentThread.ManagedThreadId.ToString() + " закончил работу");
+        }
+        public void BeginSynchronizationTransaction()
+        {
+            lock (lockObject)
+            {
+                Console.WriteLine(Thread.CurrentThread.ManagedThreadId.ToString() + " начинает работу");
+                Random random = new Random();
+                for (int i = 0; i < 15; i++)
+                {
+                    int randomNumber = random.Next(100);
+                    if (randomNumber < 50)
+                        Money -= 15000;
+                    else
+                        Money += 15000;
+                    Console.WriteLine(Money);
+                }
+                Console.WriteLine(Thread.CurrentThread.ManagedThreadId.ToString() + " закончил работу");
+            }
+        }
+    }
+}
